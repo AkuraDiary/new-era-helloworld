81e40cc73e040f6d3b23d0e9babba8a2a36f5f75
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-09-27 13:47:57 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nama = "Ayam";
+        int nim = 1234;
+        char nilai = 'A';
+
+
+        System.out.println("nama : " + nama);
+
+
+//        System.out.println("Hello world!");
+    }
+}
