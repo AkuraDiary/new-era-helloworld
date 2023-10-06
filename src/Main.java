09bf0a58cedce710829fc6fd8336d53029b160fc
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-10-06 14:18:12 +0700

    pass by reference

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,41 @@
 import java.util.Scanner;
 public class Main {
+
+    public String ayam_goyeng = "ayam";
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        // PASS BY REFERENCE CUY
+        Main ayam = new Main();
+        Main bebek = ayam;
 
-        String nama = "Ayam";
-        int nim = 1234;
-        char nilai = 'A';
+        System.out.println("Before");
+        System.out.println("ayam = " + ayam.ayam_goyeng);
+        System.out.println("bebek = " + bebek.ayam_goyeng);
+        System.out.println();
 
+        ayam.ayam_goyeng = "ayamgoyeng";
 
-        System.out.println("nama : " + nama);
+        System.out.println("After");
+        System.out.println("ayam = " + ayam.ayam_goyeng);
+        System.out.println("bebek = " + bebek.ayam_goyeng);
+        System.out.println();
 
-
-//        System.out.println("Hello world!");
     }
+
+//    void useThis(){
+//        String nama = "Ayam";
+//        String namaLagi = nama;
+//
+//        System.out.println("Before");
+//        System.out.println("nama : " + nama);
+//        System.out.println("namaLagi : " + namaLagi);
+//
+//        nama = "ayamgoyeng";
+//
+//        System.out.println("After");
+//        System.out.println("nama : " + nama);
+//        System.out.println("namaLagi : " + namaLagi);
+//
+//        System.out.println();
+//    }
 }
