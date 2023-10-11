a34f34121e9471d71da3d6df70b724a3cc1db157
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-10-11 13:22:24 +0700

    upload

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 public class Main {
 
     public String ayam_goyeng = "ayam";
+    int b=42;
     public static void main(String[] args) {
 
-        // PASS BY REFERENCE CUY
+//
+//        // PASS BY REFERENCE CUY
         Main ayam = new Main();
-        Main bebek = ayam;
+        System.out.println(ayam.bebek(10));
+//        Main bebek = ayam;
+//
+//        System.out.println("Before");
+//        System.out.println("ayam = " + ayam.ayam_goyeng);
+//        System.out.println("bebek = " + bebek.ayam_goyeng);
+//        System.out.println();
+//
+//        ayam.ayam_goyeng = "ayamgoyeng";
+//
+//        System.out.println("After");
+//        System.out.println("ayam = " + ayam.ayam_goyeng);
+//        System.out.println("bebek = " + bebek.ayam_goyeng);
+//        System.out.println();
 
-        System.out.println("Before");
-        System.out.println("ayam = " + ayam.ayam_goyeng);
-        System.out.println("bebek = " + bebek.ayam_goyeng);
-        System.out.println();
+    }
 
-        ayam.ayam_goyeng = "ayamgoyeng";
-
-        System.out.println("After");
-        System.out.println("ayam = " + ayam.ayam_goyeng);
-        System.out.println("bebek = " + bebek.ayam_goyeng);
-        System.out.println();
-
+    int bebek(int b){
+        return b;
     }
 
 //    void useThis(){
